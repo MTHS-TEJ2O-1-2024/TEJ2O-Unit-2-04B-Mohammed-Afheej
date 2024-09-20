e7d73db97ec084d9766dd16db73315cdc2b2bf14
Mohammed-Afheej <mohammed.afheej@mths.ca>
2024-09-20 11:00:03 -0400

    Edited the comment and the function

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mohammed Afheej
+ * Created on: Sep 2024
+ * This program displays the current temperature.
 */
+let CurrentTemperature = input.temperature ()
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    basic.showString("The current temperature is: " + CurrentTemperature)
+})
